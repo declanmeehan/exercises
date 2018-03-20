b2154557d19ad22d793c3af1d252f0b8f36adbac
Declan <declan.meehan@gmail.com>
2018-03-20 14:16:04 -0500

    first commit recursive function

---
+++ javascript.js
@@ -0,0 +1,15 @@
+//recursive
+// let a = 1;
+// let b = 1;
+// let c = 0;
+// function recursive() {
+//   if (a < 987) {
+//     console.log(a);
+//     c = a + b;
+//     a = b;
+//     b = c;
+//     recursive();
+//   }
+// }
+// recursive();
+//function stringRev()
